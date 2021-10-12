3709d4740f1a4e92a186f1c0bbb672c72d469b20
kohei.machida <kohei.machida@a10lab.com>
2021-10-12 17:34:09 +0900

    Initial commit

---
+++ app/src/main/java/com/github/kmachida12345/mpandroidchartpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.kmachida12345.mpandroidchartpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
